2bf35402d510a1991467b9de9f5d31430759619c
Ngo Quang Tung <tung061741@stu.vinschool.edu.vn>
2025-06-12 19:10:07 +0700

    Shortened code

--- JS/script_09062025.js
+++ JS/script_09062025.js
@@ -1,256 +1,187 @@
-let danhsachhs = []
+let danhsachhs = [];
+let danhsachreset = [];
 
-function help() {
-    danhsachhs = [
-  {
-    "ma": "48A44",
-    "ten": "a",
-    "tuoi": "10",
-    "gioitinh": "nam",
-    "chieucao": "6",
-    "cannang": "156",
-    "lop": "8A",
-    "diemtb": "10"
-  },
-  {
-    "ma": "48A445",
-    "ten": "b",
-    "tuoi": "10",
-    "gioitinh": "nam",
-    "chieucao": "6",
-    "cannang": "156",
-    "lop": "8A",
-    "diemtb": "10"
-  },
-  {
-    "ma": "48A4453",
-    "ten": "a",
-    "tuoi": "10",
-    "gioitinh": "nam",
-    "chieucao": "6",
-    "cannang": "156",
-    "lop": "8A",
-    "diemtb": "8"
-  }
-]
-alert("Đã thêm vào")
+function showdata(danhsach) {
+  const tbody = document.getElementById("tbody");
+  tbody.innerHTML = "";
+
+  danhsach.forEach((item) => {
+    const tr = document.createElement("tr");
+    const mahstd = document.createElement("td");
+    const tenhstd = document.createElement("td");
+    const tuoihstd = document.createElement("td");
+    const gioitinhhstd = document.createElement("td");
+    const chieucaohstd = document.createElement("td");
+    const cannanghstd = document.createElement("td");
+    const lophstd = document.createElement("td");
+    const diemtbhstd = document.createElement("td");
+
+    mahstd.textContent = item.ma;
+    tenhstd.textContent = item.ten;
+    tuoihstd.textContent = item.tuoi;
+    gioitinhhstd.textContent = item.gioitinh;
+    chieucaohstd.textContent = item.chieucao;
+    cannanghstd.textContent = item.cannang;
+    lophstd.textContent = item.lop;
+    diemtbhstd.textContent = item.diemtb;
+
+    tr.append(
+      mahstd,
+      tenhstd,
+      gioitinhhstd,
+      tuoihstd,
+      chieucaohstd,
+      cannanghstd,
+      lophstd,
+      diemtbhstd
+    );
+    tbody.append(tr);
+  });
 }
- 
+
 function quanlyhocsinh() {
-    const mahs = document.getElementById('mahocsinh').value
-    const tenhs = document.getElementById('tenhocsinh').value
-    const tuoihs = document.getElementById('tuoihocsinh').value
-    const gioitinhhs = document.getElementById('chongioitinh').value
-    const chieucaohs = document.getElementById('chieucaohocsinh').value
-    const cannanghs = document.getElementById('cannanghocsinh').value
-    const lophs = document.getElementById('lophocsinh').value
-    const diemtbhs = document.getElementById('diemtbhocsinh').value
+  const mahs = document.getElementById('mahocsinh').value
+  const tenhs = document.getElementById('tenhocsinh').value
+  const tuoihs = document.getElementById('tuoihocsinh').value
+  const gioitinhhs = document.getElementById('chongioitinh').value
+  const chieucaohs = document.getElementById('chieucaohocsinh').value
+  const cannanghs = document.getElementById('cannanghocsinh').value
+  const lophs = document.getElementById('lophocsinh').value
+  const diemtbhs = document.getElementById('diemtbhocsinh').value
+  let check = false;
 
-    const tbody = document.getElementById('tbody')
-    let check = false
+  const tr = document.createElement("tr");
+  const mahstd = document.createElement("td");
+  const tenhstd = document.createElement("td");
+  const tuoihstd = document.createElement("td");
+  const gioitinhhstd = document.createElement("td");
+  const chieucaohstd = document.createElement("td");
+  const cannanghstd = document.createElement("td");
+  const lophstd = document.createElement("td");
+  const diemtbhstd = document.createElement("td");
 
-    const tr = document.createElement('tr')
-    const mahstd = document.createElement('td')
-    const tenhstd = document.createElement('td')
-    const tuoihstd = document.createElement('td')
-    const gioitinhhstd = document.createElement('td')
-    const chieucaohstd = document.createElement('td')
-    const cannanghstd = document.createElement('td')
-    const lophstd = document.createElement('td')
-    const diemtbhstd = document.createElement('td')
+  let thongtinhs = {
+    ma: mahs,
+    ten: tenhs,
+    tuoi: tuoihs,
+    gioitinh: gioitinhhs,
+    chieucao: chieucaohs,
+    cannang: cannanghs,
+    lop: lophs,
+    diemtb: diemtbhs,
+  };
 
-    mahstd.textContent = mahs
-    tenhstd.textContent = tenhs
-    tuoihstd.textContent = tuoihs
-    gioitinhhstd.textContent = gioitinhhs
-    chieucaohstd.textContent = chieucaohs
-    cannanghstd.textContent = cannanghs
-    lophstd.textContent = lophs
-    diemtbhstd.textContent = diemtbhs
+  mahstd.textContent = mahs;
+  tenhstd.textContent = tenhs;
+  tuoihstd.textContent = tuoihs;
+  gioitinhhstd.textContent = gioitinhhs;
+  chieucaohstd.textContent = chieucaohs;
+  cannanghstd.textContent = cannanghs;
+  lophstd.textContent = lophs;
+  diemtbhstd.textContent = diemtbhs;
 
-    let thongtinhs = {
-        ma: mahs,
-        ten: tenhs,
-        tuoi: tuoihs,
-        gioitinh: gioitinhhs,
-        chieucao: chieucaohs,
-        cannang: cannanghs,
-        lop: lophs,
-        diemtb: diemtbhs
+  danhsachhs.forEach((item) => {
+    if (item.ma === mahs) {
+      check = true;
     }
+  });
 
-    danhsachhs.forEach (item => {
-        if (item.ma === mahs) {
-            check = true
-        }
-    })
-
-    if (check === false) {
-        tr.append(mahstd, tenhstd, gioitinhhstd, tuoihstd, chieucaohstd, cannanghstd, lophstd, diemtbhstd)
-        tbody.append(tr)
-        danhsachhs.push(thongtinhs)
-    }
-    else if (check === true) {
-        alert("Thong tin khong hop le")
-    }
+  if (check === false) {
+    tr.append(
+      mahstd,
+      tenhstd,
+      gioitinhhstd,
+      tuoihstd,
+      chieucaohstd,
+      cannanghstd,
+      lophstd,
+      diemtbhstd
+    );
+    tbody.append(tr);
+    danhsachhs.push(thongtinhs);
+    danhsachreset.push(thongtinhs);
+  } else if (check === true) {
+    alert("Thong tin khong hop le");
+  }
 }
 
 function lochocsinh() {
-    const luachon = document.getElementById('luachon').value
-    let danhsachhsfilter = []
-    if (luachon == 'luachonlop8A') {
-        danhsachhsfilter = danhsachhs.filter(item => item.lop === '8A')
-    }
-    else if (luachon == 'luachonlop8B') {
-        danhsachhsfilter = danhsachhs.filter(item => item.lop === '8B')
-    }
-    else if (luachon == 'luachontatca') {
-        danhsachhsfilter = danhsachhs
-    }
-    const tbody = document.getElementById('tbody')
-    tbody.innerHTML = ''
-
-    danhsachhsfilter.forEach(item => {
-        const tr = document.createElement('tr')
-        const mahstd = document.createElement('td')
-        const tenhstd = document.createElement('td')
-        const tuoihstd = document.createElement('td')
-        const gioitinhhstd = document.createElement('td')
-        const chieucaohstd = document.createElement('td')
-        const cannanghstd = document.createElement('td')
-        const lophstd = document.createElement('td')
-        const diemtbhstd = document.createElement('td')
-
-        mahstd.textContent = item.ma
-        tenhstd.textContent = item.ten
-        tuoihstd.textContent = item.tuoi
-        gioitinhhstd.textContent = item.gioitinh
-        chieucaohstd.textContent = item.chieucao
-        cannanghstd.textContent = item.cannang
-        lophstd.textContent = item.lop
-        diemtbhstd.textContent = item.diemtb
-
-        tr.append(mahstd, tenhstd, gioitinhhstd, tuoihstd, chieucaohstd, cannanghstd, lophstd, diemtbhstd)
-        tbody.append(tr)
-        })
+  const luachon = document.getElementById("luachon").value;
+  let danhsachhsfilter = [];
+  if (luachon == "luachonlop8A") {
+    danhsachhsfilter = danhsachhs.filter((item) => item.lop === "8A");
+  } else if (luachon == "luachonlop8B") {
+    danhsachhsfilter = danhsachhs.filter((item) => item.lop === "8B");
+  } else if (luachon == "luachontatca") {
+    danhsachhsfilter = danhsachhs;
+  }
+ showdata(danhsachhsfilter)
 }
 
-const sortNameAZ = (danhsachhs) => {
-  return danhsachhs.sort((a,b) => {
-    if(a.ten < b.ten)
-      return -1
-    if(a.ten > b.ten)
-      return 1
-    return 0
-  })
-}
+const sortNameAZ = (list) => {
+  return list.sort((a, b) => {
+    if (a.ten < b.ten) return -1;
+    if (a.ten > b.ten) return 1;
+    return 0;
+  });
+};
 
-const sortNameZA = (danhsachhs) => {
-  return danhsachhs.sort((a,b) => {
-    if(a.ten > b.ten)
-      return -1
-    if(a.ten < b.ten)
-      return 1
-    return 0
-  })
-}
+const sortNameZA = (list) => {
+  return list.sort((a, b) => {
+    if (a.ten > b.ten) return -1;
+    if (a.ten < b.ten) return 1;
+    return 0;
+  });
+};
 
 function sapxephocsinh() {
-    const sapxep = document.getElementById('sapxep').value
-    let danhsachhssort = []
-    if (sapxep == 'sapXepTenASC') {
-        danhsachhssort = sortNameAZ(danhsachhs)
-    }
-    else if (sapxep == 'sapXepTenDEC') {
-        danhsachhssort = sortNameZA(danhsachhs)
-    }
-    else if (sapxep == 'sapXepDTBasc') {
-        danhsachhssort = danhsachhs.sort((a, b) => a.diemtb-b.diemtb)
-    }
-    else if (sapxep == 'sapXepDTBdec') {
-        danhsachhssort = danhsachhs.sort((a, b) => b.diemtb-a.diemtb)
-    }
+  let danhsachsapxep = danhsachhs;
+  const sapxep = document.getElementById("sapxep").value;
 
-    const tbody = document.getElementById('tbody')
-    tbody.innerHTML = ''
+  if (sapxep == "sapXepTenASC") {
+    danhsachsapxep = sortNameAZ(danhsachsapxep);
+  } else if (sapxep == "sapXepTenDEC") {
+    console.log(danhsachsapxep);
+    danhsachsapxep = sortNameZA(danhsachsapxep);
+  } else if (sapxep == "sapXepDTBasc") {
+    danhsachsapxep = danhsachsapxep.sort((a, b) => a.diemtb - b.diemtb);
+  } else if (sapxep == "sapXepDTBdec") {
+    danhsachsapxep = danhsachsapxep.sort((a, b) => b.diemtb - a.diemtb);
+  }
 
-    danhsachhssort.forEach(item => {
-        const tr = document.createElement('tr')
-        const mahstd = document.createElement('td')
-        const tenhstd = document.createElement('td')
-        const tuoihstd = document.createElement('td')
-        const gioitinhhstd = document.createElement('td')
-        const chieucaohstd = document.createElement('td')
-        const cannanghstd = document.createElement('td')
-        const lophstd = document.createElement('td')
-        const diemtbhstd = document.createElement('td')
+  const tbody = document.getElementById("tbody");
+  tbody.innerHTML = "";
 
-        mahstd.textContent = item.ma
-        tenhstd.textContent = item.ten
-        tuoihstd.textContent = item.tuoi
-        gioitinhhstd.textContent = item.gioitinh
-        chieucaohstd.textContent = item.chieucao
-        cannanghstd.textContent = item.cannang
-        lophstd.textContent = item.lop
-        diemtbhstd.textContent = item.diemtb
-
-        tr.append(mahstd, tenhstd, gioitinhhstd, tuoihstd, chieucaohstd, cannanghstd, lophstd, diemtbhstd)
-        tbody.append(tr)
-        })
+  showdata(danhsachsapxep)
 }
 
 function sapxephs2loai() {
-    const sapxepten = document.getElementById('sapxep2loaiten').value
-    const sapxepdiem = document.getElementById('sapxep2loaidiem').value
+  const sapxepten = document.getElementById("sapxep2loaiten").value;
+  const sapxepdiem = document.getElementById("sapxep2loaidiem").value;
 
-    let danhsachsort = danhsachhs
+  let danhsachsort = danhsachhs;
 
-    if (sapxepdiem === 'sapXepDTBasc') {
-    danhsachsort.sort((a, b) => a.diemtb - b.diemtb)
-    } 
-    else if (sapxepdiem === 'sapXepDTBdec') {
-      danhsachsort.sort((a, b) => b.diemtb - a.diemtb)
-    }
+  if (sapxepdiem === "sapXepDTBasc") {
+    danhsachsort.sort((a, b) => a.diemtb - b.diemtb);
+  } else if (sapxepdiem === "sapXepDTBdec") {
+    danhsachsort.sort((a, b) => b.diemtb - a.diemtb);
+  }
 
-    if (sapxepten === "sapXepTenASC") {
-      danhsachsort = sortNameAZ(danhsachsort)
-    } 
-    else if (sapxepten === "sapXepTenDEC") {
-      danhsachsort = sortNameZA(danhsachsort)
-    }
+  if (sapxepten === "sapXepTenASC") {
+    danhsachsort = sortNameAZ(danhsachsort);
+  } else if (sapxepten === "sapXepTenDEC") {
+    danhsachsort = sortNameZA(danhsachsort);
+  }
 
+  const tbody = document.getElementById("tbody");
+  tbody.innerHTML = "";
 
-    const tbody = document.getElementById('tbody')
-    tbody.innerHTML = ''
-
-    danhsachsort.forEach(item => {
-        const tr = document.createElement('tr')
-        const mahstd = document.createElement('td')
-        const tenhstd = document.createElement('td')
-        const tuoihstd = document.createElement('td')
-        const gioitinhhstd = document.createElement('td')
-        const chieucaohstd = document.createElement('td')
-        const cannanghstd = document.createElement('td')
-        const lophstd = document.createElement('td')
-        const diemtbhstd = document.createElement('td')
-
-        mahstd.textContent = item.ma
-        tenhstd.textContent = item.ten
-        tuoihstd.textContent = item.tuoi
-        gioitinhhstd.textContent = item.gioitinh
-        chieucaohstd.textContent = item.chieucao
-        cannanghstd.textContent = item.cannang
-        lophstd.textContent = item.lop
-        diemtbhstd.textContent = item.diemtb
-
-        tr.append(mahstd, tenhstd, gioitinhhstd, tuoihstd, chieucaohstd, cannanghstd, lophstd, diemtbhstd)
-        tbody.append(tr)
-        })
+  showdata(danhsachsort)
 }
 
 function reset() {
-  const tbody = document.getElementById('tbody')
+  let tbody = document.getElementById("tbody");
+  tbody.innerHTML = "";
 
-  danhsachhs = []
-  tbody.innerHTML = ''
+  showdata(danhsachreset)
 }
